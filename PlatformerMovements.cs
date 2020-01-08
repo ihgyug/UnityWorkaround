bb94cf1eb341ef3dc3211ef7e3bda5b5f30f4e14
DaniilAlexandrov <54907170+DaniilAlexandrov@users.noreply.github.com>
2020-01-08 12:04:45 +0300

    Add files via upload

---
+++ PlatformerMovements.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerMovements : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D body;
+    private bool facingRight = true;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(moveInput * speed, body.velocity.y);
+        FlipHandler();
+    }
+    
+    void FlipHandler()
+    {
+        void Flip()
+        {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+         }
+        if (facingRight == false && moveInput > 0)
+            {
+            Flip();
+        } else if (facingRight == true && moveInput< 0)
+        {
+            Flip();
+        }
+    }
+}
---
+++ TopDownMovementSctipt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovementSctipt: MonoBehaviour
+{
+    public float speed = 1f;
+    private Vector2 moveVelocity;
+
+    private Rigidbody2D body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        body.MovePosition(body.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
