f9ee01edcb0994f5f7727db9846ac407336c8543
DaniilAlexandrov <54907170+DaniilAlexandrov@users.noreply.github.com>
2020-01-08 23:27:06 +0300

    Modular and generic.

---
+++ Health.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numberOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        if (health > numberOfHearts)
+        {
+            health = numberOfHearts;
+        }
+        if (health < 0)
+        {
+            health = 0;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            } else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            } else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.M))
+        {
+            health++;
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            health--;
+        } */
+    }
+}
