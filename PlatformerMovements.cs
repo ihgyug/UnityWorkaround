aea4120cb13eb7ed6a76e6f84921e7a1a082396a
DaniilAlexandrov <54907170+DaniilAlexandrov@users.noreply.github.com>
2020-01-08 13:14:40 +0300

    Add files via upload

--- PlatformerMovements.cs
+++ PlatformerMovements.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class PlatformerMovements : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed;
     public float jumpForce;
     private float moveInput;
 
     private Rigidbody2D body;
     private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundcheck;
+    public Transform topLeft;
+    public Transform bottomRight;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpAmount;
     void Start()
     {
+        extraJumps = extraJumpAmount;
         body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpAmount;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            body.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            body.velocity = Vector2.up * jumpForce;
+        }
+        /*if (isGrounded == true)
+        {
+            Debug.Log("You're grounded");
+        }*/
     }
     private void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(topLeft.position, bottomRight.position, whatIsGround);
+
         moveInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(moveInput * speed, body.velocity.y);
         FlipHandler();
